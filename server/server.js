3410fe715efb5b5909a2ebb0904c9ea134398b14
Dealbisac <chand.dipendra19@gmail.com>
2020-10-30 16:19:15 +0545

    model updated

--- server/server.js
+++ server/server.js
@@ -1,8 +1,3 @@
-// Copyright IBM Corp. 2016,2019. All Rights Reserved.
-// Node module: loopback-workspace
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
 'use strict';
 
 const loopback = require('loopback');
